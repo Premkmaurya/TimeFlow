52c54338157ab9f2d5e93486d4372c67fda7745b
premkmaurya <premkmaurya123456@gmail.com>
2026-04-26 14:51:01 +0530

    feat: initialize server entry point with database connection and environment configuration

--- server/server.js
+++ server/server.js
@@ -1,17 +1,15 @@
-const app = require('./src/app');
-const dotenv = require('dotenv');
-const connectDB = require('./src/config/db');
+const app = require("./src/app");
+const dotenv = require("dotenv");
+const connectDB = require("./src/config/db");
 
 dotenv.config();
 
 // Connect to Database
 connectDB();
 
-if (process.env.NODE_ENV !== 'production') {
-    const PORT = process.env.PORT || 5000;
-    app.listen(PORT, () => {
-        console.log(`🚀 Server is running on port ${PORT}`);
-    });
-}
 
-module.exports = app;
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`🚀 Server is running on port ${PORT}`);
+});
